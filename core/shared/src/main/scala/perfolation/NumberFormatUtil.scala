c35b2fd2ab7cc219ca514b7084b56ecd94d249a2
David Graham Biggs <biggs.dg@gmail.com>
2018-10-11 12:09:05 -0400

    Replaced conditional statements with pattern matching in NumberFormatUtil to increase readability.

--- core/shared/src/main/scala/perfolation/NumberFormatUtil.scala
+++ core/shared/src/main/scala/perfolation/NumberFormatUtil.scala
@@ -5,28 +5,18 @@ object NumberFormatUtil {
   def int(i: Int, digits: Int): String = {
     val s = i.toString
     val padTo = digits - s.length
-    if (padTo <= 0) {
-      s
-    } else if (padTo == 1) {
-      "0".concat(s)
-    } else if (padTo == 2) {
-      "00".concat(s)
-    } else if (padTo == 3) {
-      "000".concat(s)
-    } else if (padTo == 4) {
-      "0000".concat(s)
-    } else if (padTo == 5) {
-      "00000".concat(s)
-    } else if (padTo == 6) {
-      "000000".concat(s)
-    } else if (padTo == 7) {
-      "0000000".concat(s)
-    } else if (padTo == 8) {
-      "00000000".concat(s)
-    } else if (padTo == 9) {
-      "000000000".concat(s)
-    } else {
-      throw new RuntimeException(s"intFormat padding not available for $digits!")
+    padTo match {
+      case _ if `padTo` <= 0 => s
+      case 1 => "0".concat(s)
+      case 2 => "00".concat(s)
+      case 3 => "000".concat(s)
+      case 4 => "0000".concat(s)
+      case 5 => "00000".concat(s)
+      case 6 => "000000".concat(s)
+      case 7 => "0000000".concat(s)
+      case 8 => "00000000".concat(s)
+      case 9 => "000000000".concat(s)
+      case _ => throw new RuntimeException(s"intFormat padding not available for $digits!")
     }
   }
 }
