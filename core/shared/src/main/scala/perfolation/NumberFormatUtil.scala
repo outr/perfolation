98c664f746b00cf3cda2f58753421995bec818f7
Matt Hicks <matt@outr.com>
2018-10-11 13:41:44 -0500

    Fix to padding functionality in NumberFormatUtil to support all lengths

--- core/shared/src/main/scala/perfolation/NumberFormatUtil.scala
+++ core/shared/src/main/scala/perfolation/NumberFormatUtil.scala
@@ -1,13 +1,21 @@
 package perfolation
 
 object NumberFormatUtil {
-
   def int(i: Int, digits: Int): String = {
     val s = i.toString
     val padTo = digits - s.length
-    def zeroes(n: Int): String = if (n > 0) "0".concat(zeroes(n-1)) else ""
-
-    if (padTo >= 0) zeroes(padTo).concat(s)
-    else throw new RuntimeException(p"intFormat padding not available for $digits!")
+    padTo match {
+      case _ if `padTo` <= 0 => s
+      case 1 => "0".concat(s)
+      case 2 => "00".concat(s)
+      case 3 => "000".concat(s)
+      case 4 => "0000".concat(s)
+      case 5 => "00000".concat(s)
+      case 6 => "000000".concat(s)
+      case 7 => "0000000".concat(s)
+      case 8 => "00000000".concat(s)
+      case 9 => "000000000".concat(s)
+      case _ => "".padTo(padTo, '0')
+    }
   }
 }
