d1a66f24cee216734900c77b7f88b82079e575b6
Breakline7 <biggs.dg@gmail.com>
2018-10-23 13:02:49 -0400

    timeZoneOffsetMillis working and import statement cleaning

--- core/native/src/main/scala/perfolation/NativeCrossDate.scala
+++ core/native/src/main/scala/perfolation/NativeCrossDate.scala
@@ -1,28 +1,32 @@
 package perfolation
 
 import scala.scalanative.posix.timeOps.tmOps
-import scala.math._
 import scala.scalanative.native
-import scala.scalanative.native.{CLong, Ptr}
-import scala.scalanative.posix.time.{gmtime, localtime, mktime, time_t, tm}
+import scala.scalanative.native.Ptr
+import scala.scalanative.posix.time.{gmtime, localtime, time_t}
 
 
-class NativeCrossDate(override val milliseconds: Long, d: tmOps) extends CrossDate {
+class NativeCrossDate(override val milliseconds: Long, dms: Ptr[time_t]) extends CrossDate {
 
-//  val bmsptr: Ptr[time_t] = native.stackalloc[time_t]
-//  !bmsptr = milliseconds
-//  val g: tmOps = new tmOps(gmtime(bmsptr).asInstanceOf[Ptr[tm]])
+  //
+  val date = new tmOps(localtime(dms))
 
-  override def hour24: Int = d.tm_hour
-  override def minuteOfHour: Int = d.tm_min
-  override def secondOfMinute: Int = d.tm_sec
+  val bmsptr: Ptr[time_t] = native.stackalloc[time_t]
+  !bmsptr = milliseconds / 1000L
+  val g: tmOps = new tmOps(gmtime(bmsptr))
+  val gm: Long = 31540000000L * g.tm_year + 86400000L * g.tm_yday + 3600000L * g.tm_hour + 60000L * g.tm_min + 1000 * g.tm_sec
+  val dm: Long = 31540000000L * date.tm_year + 86400000L * date.tm_yday + 3600000L * date.tm_hour + 60000L * date.tm_min + 1000 * date.tm_sec
+
+  override def hour24: Int = date.tm_hour
+  override def minuteOfHour: Int = date.tm_min
+  override def secondOfMinute: Int = date.tm_sec
   override def milliOfSecond: Int = (milliseconds % 1000L).asInstanceOf[Int]
   override def isAM: Boolean = hour24 < 12
-  override def timeZoneOffsetMillis: Int = 14400000 //abs(mktime(gmtime(bmsptr).asInstanceOf[Ptr[tm]]).asInstanceOf[CLong] - mktime(localtime(bmsptr).asInstanceOf[Ptr[tm]]).asInstanceOf[CLong]).asInstanceOf[Int]
-  override def year: Int = d.tm_year + 1900
-  override def month: Int = d.tm_mon
-  override def dayOfWeek: Int = d.tm_wday + 1
-  override def dayOfMonth: Int = d.tm_mday
-  override def dayOfYear: Int = d.tm_yday
+  override def timeZoneOffsetMillis: Int = (gm - dm).asInstanceOf[Int]
+  override def year: Int = date.tm_year + 1900
+  override def month: Int = date.tm_mon
+  override def dayOfWeek: Int = date.tm_wday + 1
+  override def dayOfMonth: Int = date.tm_mday
+  override def dayOfYear: Int = date.tm_yday
   override def timeZone: String = "asdf"
 }
--- core/native/src/main/scala/perfolation/Platform.scala
+++ core/native/src/main/scala/perfolation/Platform.scala
@@ -1,21 +1,15 @@
 package perfolation
 
 import scala.scalanative.native
-import scala.scalanative.native.{CLong, Ptr}
 import scala.scalanative.posix.time._
-import scala.scalanative.posix.timeOps.tmOps
 
 
 object Platform {
   def createDate(l: Long): CrossDate = {
 
-//    val msptr = native.stackalloc[CLong]
-//    !msptr = l
-
     val bmsptr = native.stackalloc[time_t]
     !bmsptr = l / 1000L
-    val d = new tmOps(localtime(bmsptr).asInstanceOf[Ptr[tm]])
 
-    new NativeCrossDate(l, d)
+    new NativeCrossDate(l, bmsptr)
   }
 }
