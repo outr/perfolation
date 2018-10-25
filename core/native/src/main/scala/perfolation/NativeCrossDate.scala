67f8a61de0e0721c6372e997ebf0fbf4ac7ce649
Breakline7 <biggs.dg@gmail.com>
2018-10-25 16:16:49 -0400

    Fixed timeZone method in NativeCrossDate.scala

--- core/native/src/main/scala/perfolation/NativeCrossDate.scala
+++ core/native/src/main/scala/perfolation/NativeCrossDate.scala
@@ -2,31 +2,33 @@ package perfolation
 
 import scala.scalanative.posix.timeOps.tmOps
 import scala.scalanative.native
-import scala.scalanative.native.Ptr
-import scala.scalanative.posix.time.{gmtime, localtime, time_t}
+import scala.scalanative.native.{CString, CStruct2, Ptr}
+import scala.scalanative.posix.time._
 
 
 class NativeCrossDate(override val milliseconds: Long, dms: Ptr[time_t]) extends CrossDate {
 
-  //
+  // tmOps object that allows us to access localtime data
+  tzset
   val date = new tmOps(localtime(dms))
 
-  val bmsptr: Ptr[time_t] = native.stackalloc[time_t]
-  !bmsptr = milliseconds / 1000L
-  val g: tmOps = new tmOps(gmtime(bmsptr))
-  val gm: Long = 31540000000L * g.tm_year + 86400000L * g.tm_yday + 3600000L * g.tm_hour + 60000L * g.tm_min + 1000 * g.tm_sec
-  val dm: Long = 31540000000L * date.tm_year + 86400000L * date.tm_yday + 3600000L * date.tm_hour + 60000L * date.tm_min + 1000 * date.tm_sec
+  val tz: Ptr[CStruct2[CString, CString]] = tzname()
+
+  val gmptr: Ptr[time_t] = native.stackalloc[time_t]
+  !gmptr = milliseconds / 1000L
+  // tmpOps object that allows us to access UTC time data
+  val g: tmOps = new tmOps(gmtime(gmptr))
 
   override def hour24: Int = date.tm_hour
   override def minuteOfHour: Int = date.tm_min
   override def secondOfMinute: Int = date.tm_sec
   override def milliOfSecond: Int = (milliseconds % 1000L).asInstanceOf[Int]
   override def isAM: Boolean = hour24 < 12
-  override def timeZoneOffsetMillis: Int = (gm - dm).asInstanceOf[Int]
+  override def timeZoneOffsetMillis: Int = 1000 * timezone().asInstanceOf[Int]
   override def year: Int = date.tm_year + 1900
   override def month: Int = date.tm_mon
   override def dayOfWeek: Int = date.tm_wday + 1
   override def dayOfMonth: Int = date.tm_mday
   override def dayOfYear: Int = date.tm_yday
-  override def timeZone: String = "asdf"
+  override def timeZone: String = native.fromCString(!tz._1)
 }
