63e98700e84835de1c4ab5364652a3fd5f31c6ba
Breakline7 <biggs.dg@gmail.com>
2018-10-25 16:24:57 -0400

    Cleaned up code resulting code artifacts from functionality streamlining.

--- core/native/src/main/scala/perfolation/NativeCrossDate.scala
+++ core/native/src/main/scala/perfolation/NativeCrossDate.scala
@@ -1,23 +1,15 @@
 package perfolation
 
 import scala.scalanative.posix.timeOps.tmOps
-import scala.scalanative.native
-import scala.scalanative.native.{CString, CStruct2, Ptr}
+import scala.scalanative.native.{Ptr, fromCString}
 import scala.scalanative.posix.time._
 
 
 class NativeCrossDate(override val milliseconds: Long, dms: Ptr[time_t]) extends CrossDate {
 
-  // tmOps object that allows us to access localtime data
   tzset
+  // tmOps object that allows us to access localtime data
   val date = new tmOps(localtime(dms))
-
-  val tz: Ptr[CStruct2[CString, CString]] = tzname()
-
-  val gmptr: Ptr[time_t] = native.stackalloc[time_t]
-  !gmptr = milliseconds / 1000L
-  // tmpOps object that allows us to access UTC time data
-  val g: tmOps = new tmOps(gmtime(gmptr))
 
   override def hour24: Int = date.tm_hour
   override def minuteOfHour: Int = date.tm_min
@@ -30,5 +22,5 @@ class NativeCrossDate(override val milliseconds: Long, dms: Ptr[time_t]) extends
   override def dayOfWeek: Int = date.tm_wday + 1
   override def dayOfMonth: Int = date.tm_mday
   override def dayOfYear: Int = date.tm_yday
-  override def timeZone: String = native.fromCString(!tz._1)
+  override def timeZone: String = fromCString(!tzname._1)
 }
