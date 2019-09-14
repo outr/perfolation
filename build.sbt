cca98a8b850adf7433edd47597875a571ce2598b
Matt Hicks <matt@outr.com>
2019-09-14 09:10:30 -0500

    Release 1.1.5

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import sbtcrossproject.CrossPlugin.autoImport.crossProject
 
 name := "perfolation"
 organization in ThisBuild := "com.outr"
-version in ThisBuild := "1.1.5-SNAPSHOT"
+version in ThisBuild := "1.1.5"
 scalaVersion in ThisBuild := "2.13.0"
 crossScalaVersions in ThisBuild := List("2.12.8", "2.11.12", "2.13.0")
 scalacOptions in ThisBuild ++= Seq("-unchecked", "-deprecation")
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt +clean +compile +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +conversionJS/publishSigned +conversionJVM/publishSigned ++2.11.12 macrosNative/publishSigned coreNative/publishSigned conversionNative/publishSigned sonatypeRelease
+sbt +clean +compile +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +unitJS/publishSigned +unitJVM/publishSigned ++2.11.12 macrosNative/publishSigned coreNative/publishSigned unitNative/publishSigned sonatypeRelease
