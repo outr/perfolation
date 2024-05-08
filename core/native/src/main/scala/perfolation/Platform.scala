eabe05625c074d101c7d5242d64077a80ff11d6c
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-05-08 14:35:17 +0200

    Fix date to not start from 1970
    Fix scalatest dependency to be cross platform.
    Tests were not running on Scala Native and Scala.js

--- core/native/src/main/scala/perfolation/Platform.scala
+++ core/native/src/main/scala/perfolation/Platform.scala
@@ -1,12 +1,12 @@
 package perfolation
 
-import scala.scalanative.unsafe.stackalloc
+import scala.scalanative.unsafe._
 import scala.scalanative.posix.time.time_t
 
 object Platform {
   def createDate(l: Long): CrossDate = {
     val bmsptr = stackalloc[time_t]()
-    bmsptr(l / 1000L)
+    !bmsptr = (l / 1000L).toCSSize
 
     new NativeCrossDate(l, bmsptr)
   }
